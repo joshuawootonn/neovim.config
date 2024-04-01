7f1c8165ef6cf50aff9a601188d9c0ed18651973
Joshua Wootonn <joshuawootonn@gmail.com>
2024-04-01 08:02:31 -0400

    feat: add pop-punk colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  -- add your custom colorscheme here
+
+  { "bignimbus/pop-punk.vim" },
+  -- { "projekt0n/github-nvim-theme" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "pop-punk",
+    },
+  },
+}
