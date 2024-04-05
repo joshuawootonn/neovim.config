743b11aed2d70c962d2d5739e8b60297bc879291
joshua wootonn <joshuawootonn@gmail.com>
2024-04-04 21:33:46 -0400

    feat: change color scheme to tokyonight.night

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,14 +1,17 @@
 return {
   -- add your custom colorscheme here
 
-  { "bignimbus/pop-punk.vim" },
+  -- { "bignimbus/pop-punk.vim" },
   -- { "projekt0n/github-nvim-theme" },
 
   -- Configure LazyVim to load gruvbox
+  { "folke/tokyonight.nvim", opts = {
+    style = "night",
+  } },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "pop-punk",
+      colorscheme = "tokyonight",
     },
   },
 }
