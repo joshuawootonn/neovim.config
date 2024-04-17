301ea8bdceccc85c5a1790948ee2dd49f18f1f49
joshua wootonn <joshuawootonn@gmail.com>
2024-04-17 07:11:01 -0400

    chore: add keybindings for save all

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,4 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<leader>z", ":wa<cr>", { desc = "Save all" })
+vim.keymap.set("n", "<C-S>", ":wall<cr>", { desc = "Save all" })
+vim.keymap.set("n", "zz", ":wall<cr>", { desc = "Save all" })
+vim.keymap.set("i", "<C-S>", "<Esc>:wall<cr>i", { desc = "Save all" })
