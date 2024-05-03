38870a639dc0378d96c342ccc9cafbb7e2192437
Joshua Wootonn <joshuawootonn@gmail.com>
2024-05-03 06:43:34 -0400

    feat: adjust all keymaps for save all

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-S>", ":wall<cr>", { desc = "Save all" })
-vim.keymap.set("n", "zz", ":wall<cr>", { desc = "Save all" })
-vim.keymap.set("i", "<C-S>", "<Esc>:wall<cr>i", { desc = "Save all" })
+vim.keymap.set("n", "<leader>5", ":wall<cr>", { desc = "Save all" })
+vim.keymap.set("n", "ss", ":wall<cr>", { desc = "Save all" })
+vim.keymap.set("i", "<leader>5", "<Esc>:wall<cr>", { desc = "Save all" })
