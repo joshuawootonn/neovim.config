bee31bd4a79e086bc37876ebfec36be4813fa8bf
joshua wootonn <joshuawootonn@gmail.com>
2024-05-17 14:20:03 -0500

    feat: telescope to show --hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -18,6 +18,17 @@ return {
         sorting_strategy = "ascending",
         winblend = 20,
       },
+      pickers = {
+        find_files = {
+          hidden = true
+        },
+        grep_string = {
+          additional_args = {"--hidden"}
+        },
+        live_grep = {
+          additional_args = {"--hidden"}
+        },
+      },
     },
   },
 }
